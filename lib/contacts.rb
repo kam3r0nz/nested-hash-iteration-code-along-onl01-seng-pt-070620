dd0da05c5f4b211061e9930b5561273e99f18236
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-17 22:52:42 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,8 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts.each do |person, data|
-    puts "#{person}: #{data}""
+  contacts.each do |person, contact_details_hash|
+    binding.pry
+    puts "#{person}: #{data}"
   end
 end
